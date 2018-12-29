9f2040fd6f97f7e5f4a1e32ed09c405c25ebed97
Mehmet Tenekeci <info@mehmettenekeci.com>
2018-12-29 16:29:54 +0300

    commit after a while 5

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image} from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
-    return(
+const AlbumDetail = ({album}) => {
+
+    const {title, artist, thumbnail_image} = album; // destructured for not putting props.album. at the beginning of.
+    const {headerContentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle} = styles; //destructured for not putting styles. at the beginning of.
+
+    return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardSection>
+                <View style={thumbnailContainerStyle}>
+                    <Image style={thumbnailStyle} source={{uri: thumbnail_image}}/>
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
+            </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    }
 };
 
 export default AlbumDetail;
