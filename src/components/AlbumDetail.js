a0dcbde2f767d0f4b69e6cfe685b874ab040cd1a
Mehmet Tenekeci <info@mehmettenekeci.com>
2018-12-29 22:03:18 +0300

    finalized project

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) => {
 
-    const {title, artist, thumbnail_image} = album; // destructured for not putting props.album. at the beginning of.
-    const {headerContentStyle, headerTextStyle, thumbnailStyle, thumbnailContainerStyle} = styles; //destructured for not putting styles. at the beginning of.
+    const {title, artist, thumbnail_image, image, url} = album; // destructured for not putting props.album. at the beginning of.
+    const {
+        headerContentStyle,
+        headerTextStyle,
+        thumbnailStyle,
+        thumbnailContainerStyle,
+        imageStyle
+    } = styles; //destructured for not putting styles. at the beginning of.
 
     return (
         <Card>
@@ -18,6 +25,16 @@ const AlbumDetail = ({album}) => {
                     <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+
+            <CardSection>
+                <Image style={imageStyle} source={{uri: image}}/>
+            </CardSection>
+
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
@@ -40,6 +57,11 @@ const styles = {
         alignItems: 'center',
         marginLeft: 10,
         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
